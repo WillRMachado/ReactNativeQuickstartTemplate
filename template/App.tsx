ae410dd7b6fc680ab0373ecf55498af245aa6a6a
Will <willian.rodrigues.machado@hotmail.com>
2024-03-15 00:43:15 -0300

    update libs

--- template/App.tsx
+++ template/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { GlobalWrapper } from 'src/utils/wrappers';
 import MainScreen from './src/scenes/welcome/mainScreen/MainScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
 function App(): React.JSX.Element {
 	return (
-		<GlobalWrapper>
-			<MainScreen />
-		</GlobalWrapper>
+		<NavigationContainer>
+			<GlobalWrapper>
+				<MainScreen />
+			</GlobalWrapper>
+		</NavigationContainer>
 	);
 }
 
