7b7924bb041742425387ef3410c43ccf82f70be4
Will <willian.rodrigues.machado@hotmail.com>
2024-11-28 02:18:37 -0300

    fix: lint

--- template/src/scenes/welcome/mainScreen/MainScreen.tsx
+++ template/src/scenes/welcome/mainScreen/MainScreen.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { Box } from '@/src/gluestack/ui/box/index';
 
 function MainScreen(): React.JSX.Element {
-	return (<></>
+	return (
+		<></>
 		// <Box>
 		// 	<Text>oi</Text>
 		// 	<Text>oi</Text>
--- template/src/utils/wrappers/GlobalWrapperMocked.tsx
+++ template/src/utils/wrappers/GlobalWrapperMocked.tsx
@@ -1,9 +1,8 @@
-import { GluestackUIProvider } from '@/src/gluestack/ui/gluestack-ui-provider/index';
 import React from 'react';
 
 import { Provider } from 'react-redux';
+import { GluestackUIProvider } from '@/src/gluestack/ui/gluestack-ui-provider/index';
 import { store } from '../../store';
-
 
 function App({ children }: { children: React.ReactNode }): React.JSX.Element {
 	return (
